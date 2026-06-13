3e284a3438048fc7f8322bdf441860bf8e54a8b2
erlangga-azhar <azharerlangga2010@gmail.com>
2026-06-13 18:11:59 +0700

    rename & update file el_pipeline.yml to main.yml

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mlflow
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
@@ -8,21 +9,20 @@ def train_model():
     df = pd.read_csv("titanic_clean.csv")
     X = df.drop(columns=['survived'])
     y = df['survived']
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Tracking ke folder lokal (mlruns)
-    mlflow.set_tracking_uri("local")
-    
-    with mlflow.start_run() as run:
-        print("Melatih model...")
+    # FIX: Cek apakah ada run aktif, kalau tidak ada baru start
+    run = mlflow.active_run()
+    if run is None:
+        run = mlflow.start_run()
+        
+    with run:
+        print(f"Melatih model pada run: {run.info.run_id}")
         rf = RandomForestClassifier(random_state=42)
         rf.fit(X_train, y_train)
         
-        # Wajib log model dengan nama "model"
         mlflow.sklearn.log_model(rf, "model")
         
-        # Menyimpan Run ID untuk GitHub Actions
         with open("run_id.txt", "w") as f:
             f.write(run.info.run_id)
             
